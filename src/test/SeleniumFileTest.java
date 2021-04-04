637e6229f297d43aa9327230ea41585ed7f1aaa6
alishkad7865 <alishkadiwal@uregina.ca>
2021-04-03 22:13:17 -0600

    Selenium files

---
+++ src/test/SeleniumFileTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue; 
+
+public class SeleniumFileTest {	
+    @Test
+    public void testgooglrsearch(){
+        System.setProperty("webdriver.gecko.driver", "C:/Users/alish/Downloads/geckodriver-v0.29.0-win64/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        //it will open the goggle page
+        driver.get("https://opensource-demo.orangehrmlive.com"); 
+        //we expect the title “Google “ should be present 
+        String Expectedtitle = "OrangeHRM";
+        //it will fetch the actual title 
+        String Actualtitle = driver.getTitle();
+        System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+        //it will compare actual title and expected title
+        assertEquals(Actualtitle, Expectedtitle);
+        //print out the result
+        System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");   
+     }
+
+     @Test
+     public void alwaysTrue(){
+         assertTrue(true);
+     }
+}
