4ba50c209b61ad6a760418d7350351b42c0da9cd
alishkad7865 <alishkadiwal@uregina.ca>
2021-04-05 20:03:05 -0600

    last update

--- src/test/SeleniumFileTest.java
+++ src/test/SeleniumFileTest.java
@@ -1,10 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.net.URL;
@@ -16,7 +21,6 @@ public class SeleniumFileTest {
     WebDriver driver;
     String Expectedtitle = "";
     String Actualtitle="";
-    
     @Before
     public void setup(){
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -29,7 +33,9 @@ public class SeleniumFileTest {
 
  capabilities.setCapability("selenium_version","4.0.0-alpha-7");
          capabilities.setCapability("chrome.driver","89");
-         
+         System.setProperty("webdriver.chrome.driver","./lib/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver","./lib/geckodriver.exe");
+        driver= new FirefoxDriver();
          //driver.get("https://opensource-demo.orangehrmlive.com");
     }
 
@@ -40,16 +46,50 @@ public class SeleniumFileTest {
         WebDriver driver= new FirefoxDriver();
         driver.get("https://opensource-demo.orangehrmlive.com");
         driver.getTitle();
-        // we expect the title “Google “ should be present
         Expectedtitle = "OrangeHRM";
-        // it will fetch the actual title
         Actualtitle = driver.getTitle();
         System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
-        // it will compare actual title and expected title
         assertEquals(Actualtitle, Expectedtitle);
-        // print out the result
         System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
     }
+
+    @Test
+    public void loginTest(){
+        System.setProperty("webdriver.chrome.driver","./lib/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver","./lib/geckodriver.exe");
+        driver= new FirefoxDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com");
+
+        WebElement username=driver.findElement(By.id("txtUsername"));
+        WebElement password=driver.findElement(By.id("txtPassword"));
+        WebElement login=driver.findElement(By.id("btnLogin"));
+        
+        username.sendKeys("Admin");
+        password.sendKeys("admin123");
+        login.click();
+        String actualUrl="https://opensource-demo.orangehrmlive.com/index.php/dashboard";
+        String expectedUrl= driver.getCurrentUrl();
+        assertEquals(expectedUrl,actualUrl);
+    }
+
+    @Test
+    public void loginTest_fail(){
+        System.setProperty("webdriver.chrome.driver","./lib/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver","./lib/geckodriver.exe");
+        driver= new FirefoxDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com");
+        WebElement username=driver.findElement(By.id("txtUsername"));
+        WebElement password=driver.findElement(By.id("txtPassword"));
+        WebElement login=driver.findElement(By.id("btnLogin"));
+        
+        username.sendKeys("IAmAdmin");
+        password.sendKeys("admin123");
+        login.click();
+        String actualUrl="https://opensource-demo.orangehrmlive.com/index.php/dashboard";
+        String expectedUrl= driver.getCurrentUrl();
+        assertNotEquals(expectedUrl,actualUrl);
+    }
+
 
     @AfterClass
     public void close(){
